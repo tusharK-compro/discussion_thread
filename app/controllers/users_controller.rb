d5647aa34cd54bc278c93ec2c70b13ae87e9bc79
Tushar Kumar <tushar.kumar@comprotechnologies.com>
2025-11-06 12:30:47 +0530

    Update user controller logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,14 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
+      redirect_to posts_path
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
   private
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
